bfda5a45f209f83c7706884c7fed6fc956f4aaf7
sarojsahu <saroj.s@mindfiresolutions.com>
2022-10-05 13:39:24 +0530

    change for deploy

--- resume_parser_project/settings.py
+++ resume_parser_project/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
